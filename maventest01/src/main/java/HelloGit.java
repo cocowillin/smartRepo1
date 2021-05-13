0f5a806ba0c74fe8da7a32d4106532d424274f61
cocolin <smartxiaow@gmail.com>
2021-05-13 22:05:45 +0800

    2021年5月13日22:05:08 创建了分支

--- maventest01/src/main/java/HelloGit.java
+++ maventest01/src/main/java/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("版本1");
+        System.out.println("创建分支了 2021年5月13日22:04:44");
     }
 }
